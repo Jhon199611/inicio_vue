a7f78dd747301622ca2ba2f71a8afcf82a979183
Jhon199611 <jhon199611@gmail.com>
2019-08-22 11:03:00 -0500

    Mi firts Repository

---
+++ vue.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el:'.vue',
+    data:{
+        titulo:'Bienvenidos a vue.js',
+        src:'https://cdn-images.threadless.com/threadless-media/artist_shops/shops/vue/products/195796/shirt-1490420513-abd7b25b157f3450ce8c2a09ee51c36e.png?v=3&d=eyJvbmx5X21ldGEiOiBmYWxzZSwgImZvcmNlIjogZmFsc2UsICJvcHMiOiBbWyJ0cmltIiwgW2ZhbHNlLCBmYWxzZV0sIHt9XSwgWyJyZXNpemUiLCBbXSwgeyJ3aWR0aCI6IDk5Ni4wLCAiYWxsb3dfdXAiOiBmYWxzZSwgImhlaWdodCI6IDk5Ni4wfV0sIFsiY2FudmFzX2NlbnRlcmVkIiwgWzEyMDAsIDEyMDBdLCB7ImJhY2tncm91bmQiOiAiZmZmZmZmIn1dLCBbInJlc2l6ZSIsIFs4MDBdLCB7fV0sIFsiY2FudmFzX2NlbnRlcmVkIiwgWzgwMCwgODAwLCAiI2ZmZmZmZiJdLCB7fV0sIFsiZW5jb2RlIiwgWyJqcGciLCA4NV0sIHt9XV19'
+    },
+    methods:{
+    mostrartitulo:function(){
+    return this.titulo
+     }   
+    }
+})
+
+
+
+
+
+
+
+
+
+var app = new Vue({
+    el:'#app',
+    data:{
+        mensaje : 'aprende vue js facilmente'
+
+    }
+})
